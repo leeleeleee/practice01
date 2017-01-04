ca5c78ff1feb84d6735753db785746ac8709a62d
T3AD20 <T3AD20@G209-30>
2017-01-04 15:50:01 +0900

    dd

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -1,10 +1,17 @@
 package practice01;
 
-
 public class Prob2 {
 
 	public static void main(String[] args) {
+		int k=0;
+		for (int i = 1; i <= 7; i++) {
+			System.out.println();
+	     	k++;
+			for (int j = 0; j < 10; j++) {
+				System.out.print(j+k);
 
+			}
+		}
 	}
-
 }
+
--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -1,7 +1,28 @@
 package practice01;
 
+import java.util.Scanner;
+
 public class Prob4 {
 	public static void main(String[] args) {
+		int l=0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("문자를 입력하시오 : ");
+		String s = scanner.nextLine();
+		int length = s.length();
+		for (int i = 0; i < length; i++) {
+			System.out.println("");
+			l++;
+				for (int j = 0; j < l; j++) {
+					System.out.print(s.charAt(j));
+
+				}
+			}
 
 	}
 }
+//int k=0;
+//for (int i = 1; i <= 7; i++) {
+//	System.out.println();
+//	k++
+//	for (int j = 0; j < 10; j++) {
+//		System.out.print(j+k);
