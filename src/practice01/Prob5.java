022916d8b385fac1874fb418a1a8bc5d84f1d896
T3AD20 <T3AD20@G209-30>
2017-01-05 13:38:54 +0900

    prob5

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -3,6 +3,20 @@ package practice01;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		
+		String str = new String();
+		for (int i = 1; i < 100; i++) {
+			System.out.print(i);
+			str=String.valueOf(i);
+			int length=str.length();
+			for(int j = 0; j < length; j++) {
+				char n = str.charAt(j);
+				if( n == '3' || n == '6'|| n == '9' ) {
+					System.out.print("짝");
+				}
+			}
+			System.out.println("\n");
+		}
 
 	}
 }
